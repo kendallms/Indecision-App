9bd9e86de88f40fa083adecd77955cb6052d5070
Kendall Moffett-Sklaroff <kmoffettsklaroff@gmail.com>
2019-01-12 14:05:12 -0500

    up to webpack section

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: '/Users/kmoffettsklaroff/Personal/Indecision-App/public',
+        filename: 'bundle.js'
+    }
+}
