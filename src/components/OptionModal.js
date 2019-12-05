621a03fbf20b4a6bab0388fe3c074f2d594cb9ea
Kendall Moffett-Sklaroff <kmoffettsklaroff@gmail.com>
2019-12-05 10:14:52 -0500

    finished functionality and added minimal styling

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,6 +7,9 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleModalClose}
     contentLabel="Selected Option"
+   //  closeTimeoutMS={200}
+    className='modal'
+    ariaHideApp={false}
     >
        <h3>Selected Option</h3>
        {props.selectedOption && <p>{props.selectedOption}</p>}
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import Option from './Option';
 
-
 const Options = (props) => (
         <div> 
-        <button 
-          onClick={props.handleDeleteOptions}
-          className='button--link'
-          >
-            Remove All
-        </button>
-        {props.options.length === 0 && <p> Please add an option to get started!</p>}
+          <div className='widget-header'>
+            <h3 className='widget-header__title'>Your Options</h3>
+            <button 
+              onClick={props.handleDeleteOptions}
+              className='button--link'
+              >
+                Remove All
+            </button>
+        </div>
+        {props.options.length === 0 && <p className='widget-option'> Please add an option to get started!</p>}
         {
-            props.options.map((option) => 
+            props.options.map((option, index) => 
                 <Option 
                   key={option} 
                   optionText={option}
+                  count={index + 1}
                   handleDeleteOption={props.handleDeleteOption}
                   />
                 )
